d2cc923ec15fd03876ada6614e27468db9859418
kayweeee <tankaywee09@gmail.com>
2024-08-25 23:41:19 +0800

    layout fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,8 @@
-
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TopBar from "./components/TopBar";
 import { AuthProvider } from "./context/AuthContext";
-import amplifyConfig from '../awsConfig';
+import amplifyConfig from "../awsConfig";
 import { Amplify } from "aws-amplify";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,16 +13,12 @@ export const metadata = {
 Amplify.configure(amplifyConfig);
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
       <body className={inter.className}>
         <TopBar />
         <div className="bg-[#F6F6F6] text-[#555555] min-h-screen">
-          <AuthProvider>
-        <div className="bg-[#F6F6F6] text-[#555555] min-h-screen">
-          {children}
-          </AuthProvider>
+          <AuthProvider>{children}</AuthProvider>
         </div>
       </body>
     </html>
